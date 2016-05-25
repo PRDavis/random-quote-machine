61f96c1e17397a3471158dd92e5c84d987f61e75
Paul Davis <paulandcindy@gmail.com>
2016-05-24 23:52:59 -0400

    feat: Add ajax call
    Add ajax call to quotesondesign API.

--- dev/js/app.js
+++ dev/js/app.js
@@ -9,9 +9,52 @@ var colorGen = function(){
   return result;
 };
 
-var randColor = colorGen();
+var getNewQuote = function(){
+  var url =  'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&_jsonp=jsonpCallback';
+  $.ajax({
+    type: 'GET',
+    url: url,
+    jsonpCallback: 'jsonCallback',
+    contentType: "application/json",
+    dataType: 'jsonp',
+    success: function(data) {
+    },
+    error: function(e) {
+      console.log(e.message);
+    }
+  }
+);
+};
 
+function jsonpCallback(data){
+  var content = data[0].content;
+  var title = data[0].title;
+  title = "- " + title;
+  content = content.slice(3);
+  content = content.slice(0,-5);
+  if (content.length > 249) {
+    getNewQuote();
+  }
+  colorEverything();
+  $('.put-quote-here').html(content);
+  $('.put-attrib-here').html(title);
+}
 
+var colorEverything = function() {
+  var randColor = colorGen();
+  $(".apply-rand-color").css("color", randColor);
+  $("body").css("background-color", randColor);
+  $("button").css("background-color", randColor);
+  $("i").css("color", randColor);
+};
 
-$("body").css("background-color", randColor);
-$("i").css("color", randColor);
+var init = function(){
+  getNewQuote();
+  colorEverything();
+};
+
+$("button").click(function(){
+  getNewQuote();
+
+});
+init();
